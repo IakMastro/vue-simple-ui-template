05774e6ca4a4f9f3f8466dd4b574f0b945f8d33f
Iakovos Mastrogiannopoulos <iakmastro@outlook.com.gr>
2022-05-24 23:57:54 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Vue Template
+
+A simple, barebone Vue template made for projects that need a navbar and it's not admin.
+
+![Homepage look](homepage.png)
+
+## Technologies used
+
+* Vue 3, configured with JavaScript (no TypeScript here)
+* Bootstrap
+* VueX
+* Vue Router
+
+## Requirements
+
+* node with a package manager like npm or yarn
+
+## How to run
+
+```bash
+npm run dev # or
+yarn dev
+```
+
+## Known problems
+
+* Navbar is not 100% responsive.
+
+### Contribute
+
+Do a PR and I will manage to update it if it works. :)
+
+## License
+
+MIT - &copy; IakMastro free to use by anyone
+
+## Cheers ✌️
+
+Iakovos "IakMastro" Mastrogiannopoulos
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container-flex">
+    <nav class="navbar navbar-light">
+      <div class="dropdown d-xl-none d-lg-none mr-auto">
+        <!-- <img src="https://png.icons8.com/windows/32/000000/menu.png" data-toggle="dropdown" data-target="#navd" aria-haspopup="true" aria-expanded="false"> -->
+        <div class="dropdown-menu hb" aria-labelledby="navd">
+          <router-link class="dropdown-item" to="#">Home</router-link>
+          <router-link class="dropdown-item" to="#">About</router-link>
+        </div>
+      </div>
+      <!--Logo-->
+      <a class="navbar-brand py-0 pl-5">
+        <h1>Project Name</h1>
+      </a>
+      <!--Header navigation-->
+      <span class="navbar-item bc d-none d-xl-block d-lg-block py-0">
+        <router-link class="pl-5" to="#">Home</router-link>
+        <router-link class="px-5" to="#">About</router-link>
+      </span>
+
+      <div class="navbar-item ml-auto" style="margin-right: 1.5rem;">
+        <b-button style="margin-left: .5rem" variant="outline-danger">Logout</b-button>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+nav {
+  z-index: 100;
+}
+.navbar {
+  border-bottom: 1px solid #DCDCDC;
+  background-color: #F8F8F8;
+}
+.close {
+  position: relative;
+  bottom: 20px;
+  left: 10px;
+  font-size: 31px;
+  color: #000;
+}
+$active_color: #FFD700;
+.navbar-item.bc {
+  a {
+    font-size: 17px;
+    text-decoration: none;
+    color: black;
+    &:hover {
+      color: $active_color;
+    }
+    &:active {
+      color: $active_color;
+    }
+  }
+}
+.navbar-brand {
+  margin-left: 1.5rem;
+  user-select: none;
+}
+</style>
